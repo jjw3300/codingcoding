a51923cfcb1d61754b4dbd3f6acf226128b187cc
장진욱 <jjw3300@naver.com>
2025-08-08 12:01:53 +0900

    0808

--- 0808/Gns/1221.py
+++ 0808/Gns/1221.py
@@ -1,69 +1,18 @@
-import sys
-
-sys.stdin = open('GNS_test_input.txt')
-
 T = int(input())
 
+alien_num = {'ZRO': 0, 'ONE': 1, 'TWO': 2, 'THR': 3, 'FOR': 4,
+             'FIV': 5, 'SIX': 6, 'SVN': 7, 'EGT': 8, 'NIN': 9}
+arabic_num = {0: 'ZRO', 1: 'ONE', 2: 'TWO', 3: 'THR', 4: 'FOR',
+              5: 'FIV', 6: 'SIX', 7: 'SVN', 8: 'EGT', 9: 'NIN'}
+
 for i in range(1, T+1):
-    num, length = map(str, input().split())
-    num_length = int(length)
-    str_list = list(input().split())
-    num_list = []
-    for j in str_list:
-        if j == 'ZRO':
-            num_list.append('0')
-        elif j == 'ONE':
-            num_list.append('1')
-        elif j == 'TWO':
-            num_list.append('2')
-        elif j == 'THR':
-            num_list.append('3')
-        elif j == 'FOR':
-            num_list.append('4')
-        elif j == 'FIV':
-            num_list.append('5')
-        elif j == 'SIX':
-            num_list.append('6')
-        elif j == 'SVN':
-            num_list.append('7')
-        elif j == 'EGT':
-            num_list.append('8')
-        elif j == 'NIN':
-            num_list.append('9')
+    num, length = input().split()
+    numbers = input().split()
+    count = [0] * 10
 
-    def max_list(a, b):
-        for k in range(b - 1):
-            min_idx = k
-            for n in range(k + 1, b):
-                if a[min_idx] > a[n]:
-                    min_idx = n
-            a[k], a[min_idx] = a[min_idx], a[k]
-        return a
+    for j in numbers:
+        count[alien_num[j]] += 1
 
-    num_list = max_list(num_list, len(num_list))
-    answer_list = []
-
-    for j in num_list:
-        if j == '0':
-            answer_list.append('ZRO')
-        elif j == '1':
-            answer_list.append('ONE')
-        elif j == '2':
-            answer_list.append('TWO')
-        elif j == '3':
-            answer_list.append('THR')
-        elif j == '4':
-            answer_list.append('FOR')
-        elif j == '5':
-            answer_list.append('FIV')
-        elif j == '6':
-            answer_list.append('SIX')
-        elif j == '7':
-            answer_list.append('SVN')
-        elif j == '8':
-            answer_list.append('EGT')
-        elif j == '9':
-            answer_list.append('NIN')
-
-    print(f'#{i}')
-    print(*answer_list)
+    print(num)
+    for j in range(10):
+        print((arabic_num[j] + ' ') * count[j], end='')
