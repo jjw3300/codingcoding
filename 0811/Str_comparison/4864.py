bbab149dd981fe4eae6320dbc5f525b4be28d9d2
장진욱 <jjw3300@naver.com>
2025-08-11 17:40:07 +0900

    0811

--- 0811/Str_comparison/4864.py
+++ 0811/Str_comparison/4864.py
@@ -4,24 +4,23 @@ for tc in range(1, T + 1):
     main_word = input()
     count = 0
     i = 0
-
-    if_in = False
+    if_in = True
     while i <= len(main_word) - len(sub_word):
         if main_word[i:i + len(sub_word)][-1] != sub_word[-1]:
             i += len(sub_word)
         elif main_word[i:i + len(sub_word)][-1] == sub_word[-1]:
-            for j in range(1, len(sub_word) + 1):
+            word_count = 0
+            for j in range(1, len(sub_word)):
                 if main_word[i:i + len(sub_word)][-1 - j] == sub_word[-1 - j]:
-                    if_in = True
-                    continue
+                    word_count += 1
+                    if word_count == len(sub_word) - 1:
+                        print(f"#{tc} {1}")
+                        i = len(main_word) - len(sub_word) + 1
+                        if_in = False
                 else:
-                    if_in = False
                     i += j
                     break
         else:
             i += 1
-
     if if_in:
-        print(f"#{tc} {1}")
-    else:
         print(f"#{tc} {0}")
