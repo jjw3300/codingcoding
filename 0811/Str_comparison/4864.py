3d03a2371b2dc9d97bae2c134c5e9e315468fd76
장진욱 <jjw3300@naver.com>
2025-08-12 09:01:30 +0900

    0811

--- 0811/Str_comparison/4864.py
+++ 0811/Str_comparison/4864.py
@@ -1,26 +1,42 @@
+def bad_char_table(pattern):
+    table = {}
+    for i in range(len(pattern)):
+        table[pattern[i]] = i
+    return table
+
+
+def boyer_moore_search(text, pattern):
+
+    n = len(text)
+    m = len(pattern)
+    if m == 0:
+        return True
+
+    bad_char = bad_char_table(pattern)
+    start = 0
+    while start <= n - m:
+        j = m - 1
+
+        while j >= 0 and pattern[j] == text[start + j]:
+            j -= 1
+
+        if j < 0:
+            return True
+        else:
+            mismatch_char = text[start + j]
+            last_index = bad_char.get(mismatch_char, -1)
+            shift = max(1, j - last_index)
+            start += shift
+
+    return False
+
+
 T = int(input())
 for tc in range(1, T + 1):
     sub_word = input()
     main_word = input()
-    count = 0
-    i = 0
-    if_in = True
-    while i <= len(main_word) - len(sub_word):
-        if main_word[i:i + len(sub_word)][-1] != sub_word[-1]:
-            i += len(sub_word)
-        elif main_word[i:i + len(sub_word)][-1] == sub_word[-1]:
-            word_count = 0
-            for j in range(1, len(sub_word)):
-                if main_word[i:i + len(sub_word)][-1 - j] == sub_word[-1 - j]:
-                    word_count += 1
-                    if word_count == len(sub_word) - 1:
-                        print(f"#{tc} {1}")
-                        i = len(main_word) - len(sub_word) + 1
-                        if_in = False
-                else:
-                    i += j
-                    break
-        else:
-            i += 1
-    if if_in:
+
+    if boyer_moore_search(main_word, sub_word):
+        print(f"#{tc} {1}")
+    else:
         print(f"#{tc} {0}")
