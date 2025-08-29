9b269404abea2585e297badc5cbb2255abebb16e
장진욱 <jjw3300@naver.com>
2025-08-29 14:52:42 +0900

    0829

--- 0829/Binary_search/5176.py
+++ 0829/Binary_search/5176.py
@@ -1,26 +1,21 @@
 def inorder_traverse(node):
     global index
 
-    left = node * 2
-    right = node * 2 + 1
-
     if node > n:
         return
-    inorder_traverse(left)
 
+    inorder_traverse(node * 2)
     tree[node] = index
     index += 1
-
-    if node > n:
-        return
-    inorder_traverse(right)
+    inorder_traverse(node * 2 + 1)
 
 
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     n = int(input())
-    tree = [0 for i in range(n + 1)]
+    tree = [0] * (n + 1)
     index = 1
+
     inorder_traverse(1)
 
     root = tree[1]
