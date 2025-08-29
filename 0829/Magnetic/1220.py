1b2aeaae33634d84c365c725a0e05e1bea76fa52
장진욱 <jjw3300@naver.com>
2025-08-29 15:09:09 +0900

    0829

--- 0829/Magnetic/1220.py
+++ 0829/Magnetic/1220.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 T = 10
 for tc in range(1, T + 1):
     n = int(input())
@@ -10,14 +8,10 @@ for tc in range(1, T + 1):
             if table[i][j] == 1:
                 table[i][j] = 0
                 for k in range(i, 100):
-                    if table[k][j] == 2:
+                    if table[k][j] == 1:
+                        table[k][j] = 0
+                    elif table[k][j] == 2:
                         table[k][j] = 0
                         stalemate += 1
-                        for l in range(k, 100):
-                            if table[l][j] == 2:
-                                table[l][j] = 0
-                            elif table[l][j] == 1:
-                                break
-                    elif table[k][j] == 1:
-                        table[k][j] = 0
+                        break
     print(f"#{tc} {stalemate}")
