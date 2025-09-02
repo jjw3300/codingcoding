fd5e55521c1fb789689b77860c190ba89cb323e8
장진욱 <jjw3300@naver.com>
2025-09-02 11:39:56 +0900

    New folder

--- 0829/Cube_root/5688.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for tc in range(1, T + 1):
-    n = int(input())
-    cube_root = round(n ** (1/3))
-
-    if cube_root ** 3 != n:
-        print(f'#{tc} {-1}')
-    else:
-        print(f'#{tc} {cube_root}')
